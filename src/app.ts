77f566573e1965691f2ce66a0e331ff0624e3109
Wayne <Wayne.sheng@tuya.com>
2022-03-04 11:58:20 +0800

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import { Component } from 'react'
+import '@tarojs/taro/html5.css'
+import './app.scss'
+
+class App extends Component {
+
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentDidCatchError () {}
+
+  // this.props.children 是将要会渲染的页面
+  render () {
+    return this.props.children
+  }
+}
+
+export default App
